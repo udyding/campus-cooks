e2a2753bb90a7c3c4d7b3f3a1c003b65d34068eb
Udy Ding <udy.ding@gmail.com>
2021-02-23 16:47:08 -0500

    added minor styling

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,6 @@ import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
-import SignUp from "./SignUp";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ProfilePage from "./ProfilePage";
