34577851d24407e791617e8b5b6d75d24d779dd0
Udy Ding <udy.ding@gmail.com>
2021-02-22 23:31:14 -0500

    deploy

--- backend/config.js
+++ backend/config.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
 const DEV_CONFIG = {
-  FRONTEND_URL: "http://localhost:3000",
-  BACKEND_URL: "http://localhost:5555",
+  FRONTEND_ADDRESS: "http://localhost:3000",
+  BACKEND_ADDRESS: "http://localhost:5555",
   DATABASE_URI: "mongodb://localhost:27017/campus-cooks",
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
 const PROD_CONFIG = {
-  BACKEND_URL: process.env.BACKEND_ADDRESS,
-  FRONTEND_URL: process.env.FRONTEND_ADDRESS,
+  BACKEND_ADDRESS: process.env.BACKEND_ADDRESS,
+  FRONTEND_ADDRESS: process.env.FRONTEND_ADDRESS,
   DATABASE_URI: process.env.DATABASE_URI,
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
